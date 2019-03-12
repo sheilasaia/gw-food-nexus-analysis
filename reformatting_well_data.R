0206801490934c032af77267a014fe4e575e2f92
Sheila <sheilasaia@users.noreply.github.com>
2019-03-11 22:35:32 -0400

    reformat well data for one well

--- reformatting_well_data.R
+++ reformatting_well_data.R
@@ -3,6 +3,7 @@
 # ---- 1. load libraries ----
 
 library(tidyverse)
+library(lubridate)
 
 
 # ---- 2. read in data ----
@@ -13,16 +14,34 @@ library(tidyverse)
 raw_data_path <- "/Users/ssaia/Dropbox/GW-Food Nexus/tabular_data/raw_data/"
 
 # look for csv files
-raw_well_files_list <- list.files(path = raw_data_path, pattern = ".csv")
+well_files_list <- list.files(path = raw_data_path, pattern = ".csv")
 
 # ideas:
 # need to pull number from file name
 # loop through this list and reformat and rbind
 # maybe create function to do this all
 
-well_test_raw <- read_delim(file = paste0(raw_data_path, raw_well_files_list[1]),
+well_data_raw <- read_delim(file = paste0(raw_data_path, well_files_list[1]),
                           skip = 36,
                           col_names = FALSE, 
-                          delim = " ")
+                          delim = " ", 
+                          col_types = cols(.default = "c"))
 
-gsub("[\xca]", " ", well_test_raw$X5[1])
+well_data <- well_data_raw %>%
+  mutate(agency_code = X1,
+         station_id = X2,
+         parameter_code = X3,
+         statistic_code = X4,
+         X5_temp = gsub(" ", ",", 
+                       gsub("      ", ",", 
+                            gsub("[\xca]", " ", X5))),
+         data_approval_code = str_sub(gsub("-[\xca]", "", X6), 2, 2)) %>%
+  separate(X5_temp,
+           c("date_char", "depth_to_water_level_ft_char", "qualification_code"), 
+           sep = ",", 
+           extra = "drop") %>%
+  mutate(date = mdy(date_char), 
+         depth_to_water_level_ft = as.numeric(depth_to_water_level_ft_char)) %>%
+  select(agency_code:statistic_code, date, depth_to_water_level_ft, data_approval_code)
+
+
