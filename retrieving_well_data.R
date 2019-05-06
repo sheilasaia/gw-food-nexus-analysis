6334af25c2b8e16267b3ecaada5f6ffda40f178c
Sheila <sheilasaia@users.noreply.github.com>
2019-05-06 14:44:22 -0400

    finished well data retrival script

--- retrieving_well_data.R
+++ retrieving_well_data.R
@@ -31,23 +31,64 @@ well_ids <- st_set_geometry(well_locations_shp, NULL) %>%
 # write_csv(well_ids,paste0(tabular_data_output_path, "gw_well_metadata.csv"))
 
 well_id_list <- well_ids$station_id
-well_id_list_short <- as.vector(well_id_list[1:10])
+well_id_list_short <- as.vector(well_id_list[1:5])
 
-my_site_number <- "401444093442001"
-my_statistic_code <- "00003" # mean
-my_parameter_code <- "72019" # depth to water table level (ft below land surface)
+# metadata dataframe
+metadata <- tibble(
+  station_id = character(),
+  number_observations = integer(),
+  start_date = character(),
+  end_date = character()
+)
 
+# for loop
+for(i in 1:length(well_id_list_short)) {
+  
+  # select site
+  my_site_number <- well_id_list_short[i]
+  
+  # get raw groundwater data
+  data_raw <- readNWISgwl(siteNumbers = my_site_number,
+                          startDate = "",
+                          endDate = "",
+                          convertType = FALSE)
+  
+  # if there is some data to download (i.e., if dim(data_raw))[1] > 0)
+  if (dim(data_raw)[1] > 0) {
+    data <- data_raw %>%
+      mutate(station_id = site_no,
+             date = lev_dt,
+             date_tz = lev_tz_cd,
+             depth_to_water_ft_below_surf = lev_va,
+             approval_code = lev_status_cd) %>%
+      select(station_id:approval_code)
+    
+    # save to metadata
+    new_metadata <- tibble(station_id = as.character(my_site_number),
+                           number_observations = dim(data)[1],
+                           start_date = as.character(data$date[1]),
+                           end_date = as.character(data$date[dim(data)[1]]))
+    metadata <- bind_rows(metadata, new_metadata)
+                        
+    
+    write_csv(data, paste0(tabular_data_output_path, "daily_gw_", my_site_number, ".csv"))
+  }
+  
+  # if there's no data to download
+  else {
+    # save to metadata
+    new_metadata <- tibble(station_id = my_site_number,
+                           number_observations = 0,
+                           start_date = NA,
+                           end_date = NA)
+    
+    metadata <- bind_rows(metadata, new_metadata)
+  }
+}
 
+# add other site specific metadata
+metadata_full <- left_join(metadata, well_ids, by = "station_id")
 
-test <- readNWISdv(siteNumbers = my_site_number,
-                   parameterCd = my_parameter_code,
-                   statCd = my_statistic_code,
-                   startDate = "",
-                   endDate = "") %>%
-  mutate(station_id = site_no,
-         date = as_date(Date),
-         depth_to_water_level_ft = X_72019_00003,
-         approval_code = X_72019_00003_cd) %>%
-  select(station_id:approval_code)
+# write metadata to csv
+write_csv(metadata_full, paste0(tabular_data_output_path, "daily_gw_metadata.csv"))
 
-write_csv(test,paste0(tabular_data_output_path, "daily_gw_", my_site_number, ".csv"))
