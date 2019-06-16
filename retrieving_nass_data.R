25a768e907e9a651137e874f5e42e6e17a5c6676
Sheila <sms493@cornell.edu>
2019-06-16 13:28:27 -0400

    adds comments to nass retrieval script

--- retrieving_nass_data.R
+++ retrieving_nass_data.R
@@ -1,29 +1,41 @@
 # retrieving nass data
 
+# ---- 1. set up ----
+
 # load libraries
 library(tidyverse)
+library(here)
 library(httr)
 library(jsonlite)
-library(tidycensus)
 library(purrr)
 
 # define paths
-project_path <- here::here()
+# when we put the final data on github we'll use relative paths based on the project
+# project_path <- here::here()
+
+# force direct paths for now
 tabular_data_path <- "/Users/ssaia/Dropbox/GW-Food Nexus/tabular_data/nass_data/"
 tabular_data_output_path <- "/Users/ssaia/Dropbox/GW-Food Nexus/tabular_data/nass_data/"
+
+
+# ---- 2. load county metadata ----
 
 # load county ids data (reformatted 'AGDominatedCounties.xlsx' file)
 county_ids_raw <- read.csv(paste0(tabular_data_path, "ag_dominated_counties_update05082019.csv"))
 
 # define fips codes from reformatted 'AGDominatedCounties.xlsx' file
 county_ids <- county_ids_raw %>%
-  mutate(state_fips_pad = str_pad(state_fips, 2, pad = "0"),
-         county_fips_pad = str_pad(county_fips, 3, pad = "0"),
-         fips = as.character(paste0(state_fips_pad, county_fips_pad))) %>%
-  select(fips, county_area_sqkm:county_area_under_ag_percent)
+  mutate(state_fips_pad = str_pad(state_fips, 2, pad = "0"), # pad with zeros if not 2 digits
+         county_fips_pad = str_pad(county_fips, 3, pad = "0"), #pad with zeros if not 3 digits
+         fips = as.character(paste0(state_fips_pad, county_fips_pad))) %>% # create fips key for later dataframe joining
+  select(fips, county_area_sqkm:county_area_under_ag_percent) # select only needed columns
 
-# nass key
-# need to call your nass key into Sys.Env memory
+
+# ---- 3. get nass data ----
+
+# nass api key
+# need to call your nass api key into Sys.Env memory
+# request a nass api key at: https://quickstats.nass.usda.gov/api
 # nass_key <- "<your number here>"
 
 # nass url
@@ -39,7 +51,7 @@ my_commodity_desc_expenses <- "EXPENSE TOTALS"
 
 # query start year
 # my_year <- "2017"
-# don't define this you want all years
+# don't define this if you want all years available
 
 # state of interest
 my_state <- "LA"
@@ -70,7 +82,7 @@ list_raw_data_expenses <- fromJSON(char_raw_data_expenses)
 raw_data <- pmap_dfr(list_raw_data, rbind)
 raw_data_expenses <- pmap_dfr(list_raw_data_expenses, rbind)
 
-# clean up data
+# clean up yield, area planted, and sales data
 data <- raw_data %>%
   filter(agg_level_desc == "COUNTY") %>% # only want countly level data
   filter(prodn_practice_desc == "ALL PRODUCTION PRACTICES") %>%
@@ -86,7 +98,7 @@ data <- raw_data %>%
   mutate(value_annual = as.numeric(str_remove_all(value_annual, ","))) %>%
   filter(county_name_full != "Other (Combined) Counties") # remove un-named counties
   
-# county info
+# county metadata (join nass county data with ag dominated county_ids dataframe)
 county_data <- data %>%
   select(fips:region) %>%
   distinct() %>%
@@ -97,14 +109,14 @@ yield_data <- data %>%
   filter(statistic_cat_desc == "yield" & nass_description == "corn_grain_yield_measured_in_bu_per_acre") %>%
   mutate(cat_units = paste0(statistic_cat_desc, "_", units),
          fips_year = paste0(fips, "_", year)) %>%
-  select(fips_year, nass_description, value_annual, cat_units)
+  select(fips_year, nass_description, value_annual, cat_units) # select only necessary columns
 
 # select and reformat area planted data
 area_planted_data <- data %>%
   filter(statistic_cat_desc == "area_planted") %>%
   mutate(cat_units = paste0(statistic_cat_desc, "_", units),
          fips_year = paste0(fips, "_", year)) %>%
-  select(fips_year, nass_description, value_annual, cat_units)
+  select(fips_year, nass_description, value_annual, cat_units) # select only necessary columns
 
 # select and reformat sales data
 sales_data <- data %>%
@@ -113,7 +125,7 @@ sales_data <- data %>%
          fips_year = paste0(fips, "_", year),
          cat_units = case_when(cat_units == "sales_$" ~ "sales_usd",
                                cat_units == "sales_operations" ~ "sales_num_operations")) %>%
-  select(fips_year, nass_description, value_annual, cat_units)
+  select(fips_year, nass_description, value_annual, cat_units) # select only necessary columns
 
 # select and reformat expense data
 expense_data <- raw_data_expenses %>%
@@ -138,20 +150,22 @@ expense_data <- raw_data_expenses %>%
   select(fips_year, nass_description, value_annual, cat_units)
   
 
+# ---- 4. merge and reformat nass data ----
+
 # combine yield, area planted, sales, and expense data
-merge_data <- rbind(yield_data, area_planted_data, sales_data, expense_data) %>%
-  select(-nass_description) %>%
+merge_data <- rbind(yield_data, area_planted_data, sales_data, expense_data) %>% # NOTE! all dataframes have to have the same columns & column order!
+  select(-nass_description) %>% # delete this for now
   group_by(fips_year) %>%
-  spread(key = cat_units, value = value_annual) %>%
+  spread(key = cat_units, value = value_annual) %>% # spread data to format as requested
   mutate(fips = str_sub(fips_year, 1, 5),
-         year = as.numeric(str_sub(fips_year, 7, 10))) %>%
-  ungroup(fips_year) %>%
+         year = as.numeric(str_sub(fips_year, 7, 10))) %>% # break out fips and year
+  ungroup(fips_year) %>% # ungroup to prevent errors later
   select(fips, year, yield_bu_per_acre, area_planted_acres, 
          sales_usd , sales_num_operations,
-         expenses_num_operations:expenses_usd_per_operation) %>%
-  left_join(county_data, by = "fips") %>%
-  mutate(yield_bu_per_sqkm = yield_bu_per_acre * 247.105,
-         area_planted_sqkm = area_planted_acres * (1/247.105)) %>%
+         expenses_num_operations:expenses_usd_per_operation) %>% # select only necessary columns
+  left_join(county_data, by = "fips") %>% # join county metadata
+  mutate(yield_bu_per_sqkm = yield_bu_per_acre * 247.105, 
+         area_planted_sqkm = area_planted_acres * (1/247.105)) %>% # change from acres to sqkm
   select(year, state_alpha, county_name_full, fips, region,
          ag_area_sqkm, county_area_sqkm, county_area_under_ag_percent,
          yield_bu_per_sqkm, area_planted_sqkm,
@@ -159,10 +173,17 @@ merge_data <- rbind(yield_data, area_planted_data, sales_data, expense_data) %>%
          expenses_usd, expenses_usd_per_operation, expenses_num_operations) #%>% #, expenses_paid_by_landlord_usd, expenses_paid_by_landlord_num_operations)
   #na.omit()
 
+# for now left out operations that were owned by landlords and also kept all data (not just ag dominated counties)
+# non-ag dominated counties have NA values for ag_area_sqkm and county_area_sqkm
+
+
+# ---- 5. export nass data ----
+
 # export merged data
 write_csv(merge_data, paste0(tabular_data_output_path, "la_nass_corn_data.csv"))
 
-#  extra code
+
+# ---- 6. extra code ----
 # select and reformat area harvested data
 corn_area_harvested_data <- corn_data %>%
   filter(statisticcat_desc_full == "area_harvested") %>%
@@ -179,5 +200,6 @@ corn_production_data <- corn_data %>%
   filter(statisticcat_desc_full == "production")
 
 
-# to do:
-# for expense data need to make another r script
+# ---- 7. to do: ----
+# any other variables that have better temporal coverage than expenses or sales?
+# make function and for loop for multiple states
